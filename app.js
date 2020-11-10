ce2498f3c00b8b5e2c16340704638ffe6ad53a0f
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-11 04:43:54 +0600

    updated app.js,package.json

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require(`express`);
 const mongoose = require(`mongoose`);
 const path = require(`path`);
 const morgan = require(`morgan`);
-const livereload = require(`livereload`);
-const connectLivereload = require(`connect-livereload`);
 const session = require(`express-session`);
 const mongoDbSessionStore = require('connect-mongodb-session')(session);
 require(`dotenv/config`);
@@ -18,11 +16,6 @@ var store = new mongoDbSessionStore({
     expires: 1000 * 60 * 60 * 7,
 });
 
-/* live reload server creation */
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
-
 /* set view engine */
 app.set(`views`, `views`);
 app.set(`view engine`, `ejs`);
@@ -33,19 +26,10 @@ app.use([
     express.json(),
     express.static(`public`),
     express.urlencoded({ extended: false }),
-    connectLivereload(),
     session({ secret: `octapia secret token`, store, resave: false, saveUninitialized: false }),
     routes
 ]);
 
-
-/* live reload */
-
-liveReloadServer.server.once(`connection`, () => {
-    setTimeout(() => {
-        liveReloadServer.refresh(`/`);
-    }, 100);
-});
 
 /* database connection */
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
