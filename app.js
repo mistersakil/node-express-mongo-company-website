4308b99a6f54dc1f6619ba626d8836000c2e80e0
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-12 20:14:28 +0600

    markdown added

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 1234;
 
 /* MengoDB Session store*/
 var store = new mongoDbSessionStore({
-    uri: `mongodb+srv://root:root@cluster0.btmjp.mongodb.net/blog`,
+    uri: process.env.DB_CONNECTION
+,
     collection: 'sessions',
     expires: 1000 * 60 * 60 * 7,
 });
@@ -32,7 +33,7 @@ app.use([
 
 
 /* database connection */
-mongoose.connect(`mongodb+srv://root:root@cluster0.btmjp.mongodb.net/blog`, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log(`Connected to BD`);
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
