c6824a7712d694772b35014972f2efad1998edfe
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-15 20:06:35 +0600

    make it better

--- routes.js
+++ routes.js
@@ -1,49 +1,13 @@
 const router = require(`express`).Router();
 
-const { redirectToLoginPage, redirectToAdminPage, passUserinfoToViews } = require(`./middlewares/middlewares`);
-
-/* Validators */
-
-const loginValidator = require(`./validators/loginValidator`);
-const registerValidator = require(`./validators/registerValidator`);
-
 /* Controllers */
 
 const { register, registerProcess } = require(`./controllers/RegisterController`);
-const { login, loginProcess } = require(`./controllers/LoginController`);
-const { home } = require(`./controllers/HomeController`);
-const { dashboard, posts, fourOfour } = require(`./controllers/DashboardController`);
-const { logout } = require(`./controllers/LogoutController`);
-const { profile } = require(`./controllers/ProfileController`);
-const { users } = require(`./controllers/UserController`);
-
-
-/* middlewares */
-
-router.use(`/admin`, [redirectToLoginPage]);
-router.use(`/login`, [redirectToAdminPage]);
-router.use(`/`, [passUserinfoToViews]);
 
 /* routes for frontend */
 
-router.get(`/`, home);
-router.get(`/register`, register);
-router.post(`/register`, registerValidator, registerProcess);
-router.get(`/login`, login);
-router.post(`/login`, loginValidator, loginProcess);
-
-/* routes for backend */
-router.get(`/admin`, dashboard);
-router.get(`/admin/posts`, posts);
-router.get(`/admin/logout`, logout);
-router.get(`/admin/profile`, profile);
-router.get(`/admin/users`, users);
-
-
-router.get(`/admin/*`, fourOfour);
-
-router.get(`*`, (req, res) => {
-    res.send(`not found`);
+router.get(`/`, (req, res) => {
+    res.send('home page');
 });
 
 
