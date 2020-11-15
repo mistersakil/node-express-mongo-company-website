c3c294087394924e2ecaa219001ed79eaf501be4
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-15 18:15:03 +0600

    make it better

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const express = require(`express`)
-const mongoose = require(`mongoose`)
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+const path = require(`path`);
+const morgan = require(`morgan`);
+const session = require(`express-session`);
+const mongoDbSessionStore = require('connect-mongodb-session')(session);
 require(`dotenv/config`);
-
-const app = express()
+const app = express();
+const routes = require(`./routes`);
 const PORT = process.env.PORT || 2222;
 
+/* MengoDB Session store*/
+var store = new mongoDbSessionStore({
+    uri: process.env.DB_CONNECTION,
+    collection: 'sessions',
+    expires: 1000 * 60 * 60 * 7,
+});
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+/* set view engine */
+app.set(`views`, `views`);
+app.set(`view engine`, `ejs`);
 
+/* middlewares */
+app.use([
+    morgan(`dev`),
+    express.json(),
+    express.static(`public`),
+    express.urlencoded({ extended: false }),
+    session({ secret: `octapia secret token`, store, resave: false, saveUninitialized: false }),
+    routes
 
-app.get("/", (req, res) => {
-    res.send('home page');
-})
+]);
 
 
 /* database connection */
