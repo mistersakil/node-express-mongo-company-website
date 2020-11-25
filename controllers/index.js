1b3328a96bdd8c958b49002d0c4baf10ff6d8a99
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-26 03:37:15 +0600

    updated controllers index file and controllers nameed camelCased

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 /* Controllers Frontend */
 
-const ErrorController = require(`./frontend/ErrorController`);
-const HomeController = require(`./frontend/HomeController`);
-const ContactController = require(`./frontend/ContactController`);
-const AboutController = require(`./frontend/AboutController`);
-const BlogController = require(`./frontend/BlogController`);
-const GalleryController = require(`./frontend/GalleryController`);
-const PortfolioController = require(`./frontend/PortfolioController`);
-const ServiceController = require(`./frontend/ServiceController`);
+const aboutController = require(`./frontend/aboutController`);
 const authController = require(`./frontend/authController`);
+const blogController = require(`./frontend/blogController`);
+const contactController = require(`./frontend/contactController`);
+const errorController = require(`./frontend/errorController`);
+const homeController = require(`./frontend/homeController`);
+const portfolioController = require(`./frontend/portfolioController`);
+const galleryController = require(`./frontend/galleryController`);
+const ServiceController = require(`./frontend/ServiceController`);
 
-module.exports = { frontend: { ...ErrorController, ...HomeController, ...ContactController, ...AboutController, ...BlogController, ...GalleryController, ...PortfolioController, ...ServiceController, ...authController } };
+module.exports = { frontend: { ...errorController, ...homeController, ...contactController, ...aboutController, ...blogController, ...galleryController, ...portfolioController, ...ServiceController, ...authController } };
