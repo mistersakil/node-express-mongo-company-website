ad9cad29b5f1852c5e8802aa5de4e677b8551fd1
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-26 02:38:21 +0600

    updated index.js from controllers dir

--- controllers/index.js
+++ controllers/index.js
@@ -10,11 +10,6 @@ const portfolioController = require(`./frontend/portfolioController`);
 const serviceController = require(`./frontend/serviceController`);
 const authController = require(`./frontend/authController`);
 
-/* Controllers Backend */
-
-// const dashboardBackendController = require(`./backend/dashboardController`);
-// const userBackendController = require(`./backend/userController`);
-
 module.exports = {
     frontend: { ...errorController, ...homeController, ...contactController, ...aboutController, ...blogController, ...galleryController, ...portfolioController, ...serviceController, ...authController }
 };
