9086057650e361fc9f4a40b0b350df833a26f439
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-26 03:27:54 +0600

    updated controllers index.js file

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,14 @@
 /* Controllers Frontend */
 
-const errorController = require(`./frontend/errorController`);
-const homeController = require(`./frontend/homeController`);
-const contactController = require(`./frontend/contactController`);
 const aboutController = require(`./frontend/aboutController`);
+const authController = require(`./frontend/authController`);
 const blogController = require(`./frontend/blogController`);
+const contactController = require(`./frontend/contactController`);
+const errorController = require(`./frontend/errorController`);
 const galleryController = require(`./frontend/galleryController`);
+const homeController = require(`./frontend/homeController`);
 const portfolioController = require(`./frontend/portfolioController`);
 const serviceController = require(`./frontend/serviceController`);
-const authController = require(`./frontend/authController`);
 
 module.exports = {
     frontend: { ...errorController, ...homeController, ...contactController, ...aboutController, ...blogController, ...galleryController, ...portfolioController, ...serviceController, ...authController }
