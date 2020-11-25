b2a5a5bda6041f5ea9b3cfc1bef1c317376b660c
Md. Sakil Mahmud <sakilorb@icloud.com>
2020-11-26 04:03:53 +0600

    working on backend routes and controllers

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,14 @@ const portfolioController = require(`./frontend/portfolioController`);
 const galleryController = require(`./frontend/galleryController`);
 const serviceController = require(`./frontend/serviceController`);
 
+/* Controllers Frontend */
+
+const dashboardBackendController = require(`./backend/dashboardController`);
+const userBackendController = require(`./backend/userController`);
+
 
 module.exports = {
-    frontend: { ...errorController, ...homeController, ...contactController, ...aboutController, ...blogController, ...galleryController, ...portfolioController, ...serviceController, ...authController }
+    frontend: { ...errorController, ...homeController, ...contactController, ...aboutController, ...blogController, ...galleryController, ...portfolioController, ...serviceController, ...authController },
+    backend: { ...dashboardBackendController, ...userBackendController }
+
 };
